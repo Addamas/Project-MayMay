2800a7f80e8b5897428702194c34c34e3a19fd14
Jannie <jan.vijfhuizen@outlook.com>
2018-02-06 11:24:10 +0000

    Added AI base

---
+++ Unity Project/Project-MayMay/Assets/AI/Stat.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Stat : ScriptableObject {
+
+    public bool saveChangesInPlayMode;
+    //stats can be very generic, so that's why it isn't just an int or a float
+    public abstract int GetValue();
+    public abstract void SetValue(int val);
+    public abstract void AddValue(int val);
+
+    public abstract float TimeLeftUntilEmpty();
+    public List<RootAction> boosters = new List<RootAction>();
+}
