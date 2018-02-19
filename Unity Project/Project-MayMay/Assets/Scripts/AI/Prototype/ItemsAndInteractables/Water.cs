f07819640fbfacee0fd50d2ed5bd979262247b3d
Jannie <jan.vijfhuizen@outlook.com>
2018-02-19 12:34:41 +0000

    pls

---
+++ Unity Project/Project-MayMay/Assets/Scripts/AI/Prototype/ItemsAndInteractables/Water.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : SimpleConsumable
+{
+    public override void Consume(Character character)
+    {
+        foreach (Stat stat in character.stats)
+            if (stat as Hunger != null)
+            {
+                stat.AddValue(GetFillAmount());
+                base.Consume(character);
+                return;
+            }
+    }
+}
