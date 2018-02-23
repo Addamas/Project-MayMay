8655f1577224c66e7b5d42c9f397cba0f5a08a72
Jannie <jan.vijfhuizen@outlook.com>
2018-02-23 08:49:20 +0000

    fixed satan's fuck up

---
+++ Unity Project/Project-MayMay/Assets/Scripts/AI/Prototype/FillBucket.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Jext;
+
+[CreateAssetMenu(fileName = "FillBucket", menuName = "Actions/FillBucket", order = 1)]
+public class FillBucket : GetFood
+{
+    public override void Complete()
+    {
+        ai.filledRequirements.Remove(Jai.Requirement.hasBucket);
+        ai.filledRequirements.Add(Jai.Requirement.hasFilledBucket);
+
+        base.Complete();
+    }
+}
---
+++ Unity Project/Project-MayMay/Assets/Scripts/AI/Prototype/GeneralMaintaining/Sleep.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Sleep", menuName = "Actions/Sleep", order = 1)]
+public class Sleep : SimpleRootAction
+{
+    public override void Cancel()
+    {
+        ai.StopCoroutine(execute);
+    }
+
+    public override void Execute()
+    {
+        execute = ai.StartCoroutine(_Execute());
+    }
+
+    private float start;
+    private Coroutine execute;
+    private IEnumerator _Execute()
+    {
+        start = Gamemanager.time;
+        yield return new WaitForSeconds(STAT<Quest>().GetDuration());
+        Complete();
+    }
+
+    public override int GetReturnValue()
+    {
+        return 1;
+    }
+
+    public override Vector3 Pos()
+    {
+        return GetInteractableX.SPGetAllX<Bed>(ai)[0].transform.position;
+    }
+}
