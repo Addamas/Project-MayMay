b565a6b6f072073caf2ee5def636f9f9d257f5cb
Jannie <jan.vijfhuizen@outlook.com>
2018-03-14 14:14:58 +0000

    renzo doet me pijn

--- Unity Project/Project-MayMay/Assets/Sourcefiles/_Scripts/PlayerController.cs
+++ Unity Project/Project-MayMay/Assets/Sourcefiles/_Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.AI;
 
 #region Goals
+//one day, become as great as my great senpai san, JAN <3
+
 /* Camera Controlls, 
  *  - A-D to rotate left and right around player. W-S to pan/zoom in on player (slowly goes horizontal abit)
  *  - Roof fading when inside house (Roof of building fades to 10% opacity)
@@ -18,59 +20,91 @@ using UnityEngine.AI;
 #endregion
 
 public class PlayerController : MonoBehaviour {
-    [Header("Main Camera needs to be child 0")]
-    
-    public Transform player;
-    #region Camera Functions Var's
-    private Transform cam;
-    public Transform camZoomHigh;
-    public Transform camZoomLow;
-    public float camZoomThreshold;
-    [Header("150 is the sweet spot")]
-    public float cameraRotationSpeed; //Settings var (150 is the sweet spot imo)
-    #endregion
-    #region Player Functions Var's
-    public NavMeshAgent playerAgent;
+
+    #region Player Var's
+    [SerializeField]
+    private Transform player;
+    private NavMeshAgent playerAgent;
     #endregion
 
-    void Start () {
-        player = GameObject.FindWithTag("Player").transform;
-        cam = transform.GetChild(0);
-        cam.position = camZoomHigh.position;
-        cam.rotation = camZoomHigh.rotation;
+    #region Camera Var's
+    [SerializeField]
+    private Transform cam, camZoomHigh, camZoomLow;
+    [SerializeField]
+    private float cameraRotationSpeed = 150, zoomSpeed;
+
+    [SerializeField]
+    private string scroll = "Mouse ScrollWheel", horizontal = "Horizontal";
+
+    #region Fast References
+    private Vector3 CamPos
+    {
+        get
+        {
+            return cam.transform.position;
+        }
+        set
+        {
+            cam.transform.position = value;
+        }
+    }
+
+    private Quaternion CamRot
+    {
+        get
+        {
+            return cam.rotation;
+        }
+        set
+        {
+            cam.rotation = value;
+        }
+    }
+    #endregion
+
+    #endregion
+
+    private void Awake()
+    {
         playerAgent = player.GetComponent<NavMeshAgent>();
+    }
+
+    private void Start () {
+        CamPos = camZoomHigh.position;
+        CamRot = camZoomHigh.rotation;
+
         cam.LookAt(player);
     }
-	
-	void Update () {
+
+    private float rotation;
+    private void Update () {
 
         cam.LookAt(player);
 
         #region Camera Functions
 
         #region Camera Rotation
-        float rotation = (Input.GetAxis("Horizontal") * cameraRotationSpeed) * Time.deltaTime;
+        rotation = (Input.GetAxis(horizontal) * cameraRotationSpeed) * Time.deltaTime;
         transform.Rotate(0, rotation, 0);
         //cam.LookAt(player); //overcall
         
         #endregion
 
         #region Camera Zoom
-        if (Input.GetAxis("Mouse ScrollWheel") > 0f && camZoomThreshold <= 1f)
+
+        if (Input.GetAxisRaw(scroll) != 0)
         {
-            StartCoroutine(CamZoom(0.2f));
-            Debug.Log("Zooming In");
+            if (camZoom != null)
+                StopCoroutine(camZoom);
+            camZoom = StartCoroutine(CamZoom());
         }
-        else if (Input.GetAxis("Mouse ScrollWheel") < 0f && camZoomThreshold >= 0f)
-        {
-            StartCoroutine(CamZoom(-0.2f));
-            Debug.Log("Zooming Out");
-        }
+
         #endregion
 
         #endregion
 
         #region Player Controls
+
         if (transform.position != player.position)
             transform.position = player.position;
 
@@ -97,33 +131,32 @@ public class PlayerController : MonoBehaviour {
     }
 
     #region Camera Zoom Coroutine
-    IEnumerator CamZoom(float camZoomThresholder)
+    /*
+    private void CamZoom(float speed)
     {
-        float testZoom;
-        testZoom = camZoomThreshold + camZoomThresholder;
-        Debug.Log("Testzoom = " + testZoom);
+        Transform target = speed > 0 ? camZoomLow : camZoomHigh;
+        float disPart = Mathf.Abs(speed) * zoomSpeed * Time.deltaTime;
+        CamPos = Vector3.MoveTowards(CamPos, target.position, disPart);
+    }
+    */
 
-        while (camZoomThreshold <= testZoom)
+    private Coroutine camZoom;
+    private IEnumerator CamZoom()
+    {
+        float axis = Input.GetAxis(scroll);
+        float startingDistance = Mathf.Abs(axis) * zoomSpeed, 
+            distanceLeft = startingDistance;
+        Transform target = axis > 0 ? camZoomLow : camZoomHigh;
+
+        float disPart;
+        while(distanceLeft > 0)
         {
-            cam.position = Vector3.Lerp(camZoomHigh.position, camZoomLow.position, camZoomThreshold);
+            disPart = zoomSpeed * Time.deltaTime;
+            distanceLeft -= disPart;
 
-            if (testZoom > camZoomThreshold)
-            {
-                camZoomThreshold += 0.01f;
-            }
-            yield return null;
-        }
-        while (camZoomThreshold >= testZoom)
-        {
-            cam.position = Vector3.Lerp(camZoomHigh.position, camZoomLow.position, camZoomThreshold);
-
-            if (testZoom < camZoomThreshold)
-            {
-                camZoomThreshold -= 0.01f;
-            }
+            CamPos = Vector3.MoveTowards(CamPos, target.position, disPart);
             yield return null;
         }
     }
     #endregion
-
 }
